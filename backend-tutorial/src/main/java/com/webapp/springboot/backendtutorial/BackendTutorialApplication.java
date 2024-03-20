4fe5be52b4371bab37bcf1837020e5bc7c48d621
sonya.hedider <hedider.sonya@gmail.com>
2024-03-20 19:25:27 +0100

    Tutorial: Building an Application with Spring Boot

--- backend-tutorial/src/main/java/com/webapp/springboot/backendtutorial/BackendTutorialApplication.java
+++ backend-tutorial/src/main/java/com/webapp/springboot/backendtutorial/BackendTutorialApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendTutorialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendTutorialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTutorialApplication.class, args);
+    }
 
 }
