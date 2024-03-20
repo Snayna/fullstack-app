de86192a4020a4a37216228ecbce608e5cf9a601
sonya.hedider <hedider.sonya@gmail.com>
2024-03-20 17:45:21 +0100

    Spring Boot initializer

---
+++ backend-tutorial/src/main/java/com/webapp/springboot/backendtutorial/BackendTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.springboot.backendtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTutorialApplication.class, args);
+	}
+
+}
---
+++ backend-tutorial/src/test/java/com/webapp/springboot/backendtutorial/BackendTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.springboot.backendtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
